34ee87b8275a30dce7c047784b62d16c0768079d
Susan Yoon <awesomeyooner@gmail.com>
2025-04-06 12:48:48 -0700

    test

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ViewProvider } from './util/ViewProvider';
+import { open } from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,6 +12,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "notes-viewer" is now active!');
 
+	const provider = new ViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			ViewProvider.viewType,
+			provider
+		)
+	)
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vscodeSidebar.menu.view", () => {
+			const message = "Menu/Title of extension is clicked!";
+			vscode.window.showInformationMessage(message);
+		})
+	)
+
+	let openWebView = vscode.commands.registerCommand('vscodeSidebar.openview', () => {
+		vscode.window.showInformationMessage('Command " Sidebar View [vscodeSidebar.openview] " called.');
+	});
+
+	context.subscriptions.push(openWebView);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
